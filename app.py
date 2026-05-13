2b37f53461ea989880747e823efa68955e9ff24d
bajiniteenoj <98517993+bajiniteenoj@users.noreply.github.com>
2026-05-13 08:10:06 +0000

    Removed secrets from repository

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import streamlit as st
+from openai import OpenAI
+
+client = OpenAI(
+base_url="https://openrouter.ai/api/v1",
+api_key=st.secrets["OPENROUTER_API_KEY"]
+)
 
 st.title("NeuralNova AI Study Assistant")
 
 user_input = st.text_input("Ask something:")
 
 if user_input:
-    st.write("You asked:", user_input)
+	response = client.chat.completions.create(
+		model="openai/gpt-3.5-turbo",
+		messages=[
+			{
+				"role": "system",
+				"content": "You are NeuralNova, an AI study assistant that helps students learn programming, AI, productivity, and exam preparation in a simple and friendly way."
+			},
+			{
+				"role": "user",
+				"content": user_input
+			}
+		]
+	)
 
-
-if "python" in user_input.lower():
-    st.write("Python is a powerful programming language used in AI and automation.")
-
-elif "ai" in user_input.lower():
-    st.write("Artificial Intelligence helps machines learn and make decisions.")
-
-else:
-    st.write("I'm still learning. More AI features coming soon.")
+	st.write("AI Response:")
+	st.write(response.choices[0].message.content)
