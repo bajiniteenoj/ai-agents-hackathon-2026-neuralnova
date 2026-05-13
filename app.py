659efcff8b54ea525cfaab31be80a8503903f642
bajiniteenoj <98517993+bajiniteenoj@users.noreply.github.com>
2026-05-13 07:41:52 +0000

    Built initial Streamlit AI assistant UI

--- app.py
+++ app.py
@@ -5,5 +5,14 @@ st.title("NeuralNova AI Study Assistant")
 user_input = st.text_input("Ask something:")
 
 if user_input:
-st.write("You asked:", user_input)
-st.write("AI response will come here soon.")
+    st.write("You asked:", user_input)
+
+
+if "python" in user_input.lower():
+    st.write("Python is a powerful programming language used in AI and automation.")
+
+elif "ai" in user_input.lower():
+    st.write("Artificial Intelligence helps machines learn and make decisions.")
+
+else:
+    st.write("I'm still learning. More AI features coming soon.")
